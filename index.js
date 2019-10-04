c0453d1b8fc818d0c97880d2a9ba558a748cce7b
fabrice404 <fabrice.lamant@gmail.com>
2019-10-04 21:59:59 +0100

    add eslint and prettier

--- index.js
+++ index.js
@@ -1,76 +1,92 @@
-const Table = require('cli-table');
-const moment = require('moment-timezone');
-const fs = require('fs');
-const nodeSsh = require('node-ssh');
+const Table = require("cli-table");
+const moment = require("moment-timezone");
+const fs = require("fs");
+const nodeSsh = require("node-ssh");
 const ssh = new nodeSsh();
-const path = require('path');
+const path = require("path");
 
-const CRLF = '\r\n';
+const CRLF = "\r\n";
 
-const vEVENT = (event) => {
+const vEVENT = event => {
   const icsEvent = [];
   if (event.description) {
     icsEvent.push(`DESCRIPTION:${event.description}`);
   }
-  icsEvent.push(`DTSTART:${moment(event.begin).format('YYYYMMDDTHHmmss')}`);
-  icsEvent.push(`DTEND:${moment(event.end).format('YYYYMMDDTHHmmss')}`);
+  icsEvent.push(`DTSTART:${moment(event.begin).format("YYYYMMDDTHHmmss")}`);
+  icsEvent.push(`DTEND:${moment(event.end).format("YYYYMMDDTHHmmss")}`);
 
-  icsEvent.push(`DTSTAMP:${moment().format('YYYYMMDDTHHmmss')}`);
+  icsEvent.push(`DTSTAMP:${moment().format("YYYYMMDDTHHmmss")}`);
 
   icsEvent.push(`SUMMARY:${event.icon} ${event.name}`);
 
-  icsEvent.push(`UID:${moment(event.begin).format('YYYYMMDDTHHmmss')}`);
+  icsEvent.push(`UID:${moment(event.begin).format("YYYYMMDDTHHmmss")}`);
 
   return `BEGIN:VEVENT${CRLF}${icsEvent.sort().join(CRLF)}${CRLF}END:VEVENT`;
-}
+};
 
-const vCALENDAR = (calendar) => {
-  const icsEvents = calendar.map(vEVENT)
+const vCALENDAR = calendar => {
+  const icsEvents = calendar.map(vEVENT);
 
   const icsCalendar = [];
-  icsCalendar.push('BEGIN:VCALENDAR')
-  icsCalendar.push('VERSION:2.0')
-  icsCalendar.push('PRODID:-//ics-generator//Calendar//EN')
-  icsCalendar.push(icsEvents.join(CRLF))
-  icsCalendar.push('END:VCALENDAR')
+  icsCalendar.push("BEGIN:VCALENDAR");
+  icsCalendar.push("VERSION:2.0");
+  icsCalendar.push("PRODID:-//ics-generator//Calendar//EN");
+  icsCalendar.push(icsEvents.join(CRLF));
+  icsCalendar.push("END:VCALENDAR");
 
   return icsCalendar.join(CRLF);
-}
+};
 
-const INPUT = `${__dirname}/input`
-const OUTPUT = `${__dirname}/output`
+const INPUT = `${__dirname}/input`;
+const OUTPUT = `${__dirname}/output`;
 
 fs.readdir(INPUT, (errFolder, files) => {
   if (errFolder) {
     throw errFolder;
   }
-  files.filter(file => path.extname(file).toLowerCase() === '.json' && file !== 'sample.json')
+  files
+    .filter(
+      file =>
+        path.extname(file).toLowerCase() === ".json" && file !== "sample.json"
+    )
     .forEach(file => {
-      fs.readFile(`${INPUT}/${file}`, { encoding: 'utf-8' }, (errFile, content) => {
-        if (errFile) {
-          throw errFile;
-        }
-        const name = path.normalize(file).slice(0, -5);
-        const { deploy, events } = JSON.parse(content);
-        const vcal = vCALENDAR(events);
-        const outputFile = `${OUTPUT}/${name}.ics`;
-        fs.writeFile(outputFile, vcal, (errWrite) => {
+      fs.readFile(
+        `${INPUT}/${file}`,
+        { encoding: "utf-8" },
+        (errFile, content) => {
           if (errFile) {
-            throw errWrite;
+            throw errFile;
           }
-          console.log(`${outputFile} created!`)
+          const name = path.normalize(file).slice(0, -5);
+          const { deploy, events } = JSON.parse(content);
+          const vcal = vCALENDAR(events);
+          const outputFile = `${OUTPUT}/${name}.ics`;
+          fs.writeFile(outputFile, vcal, errWrite => {
+            if (errFile) {
+              throw errWrite;
+            }
+            console.log(`${outputFile} created!`);
 
-          ssh.connect({ host, username, password, port } = deploy)
-            .then(() => {
-              ssh.putFile(outputFile, deploy.location)
-                .then(() => {
-                  console.log(`${outputFile} deployed to ${deploy.location}!`)
-                  ssh.dispose();
-                })
-                .catch(err => { throw err });
-            })
-            .catch(err => { throw err });
-        });
-      })
+            ssh
+              .connect(({ host, username, password, port } = deploy))
+              .then(() => {
+                ssh
+                  .putFile(outputFile, deploy.location)
+                  .then(() => {
+                    console.log(
+                      `${outputFile} deployed to ${deploy.location}!`
+                    );
+                    ssh.dispose();
+                  })
+                  .catch(err => {
+                    throw err;
+                  });
+              })
+              .catch(err => {
+                throw err;
+              });
+          });
+        }
+      );
     });
 });
